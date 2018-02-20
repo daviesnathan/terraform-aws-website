942c02eae3e4050f7722e8f8ccbeffb440205689
Nathan Davies <nathan.davies@devopsguys.com>
2018-02-20 16:07:34 +0000

    Updates to allow additional access sources and addition of dev server

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-aws-remote-state"
-    key = "terraform-aws-website.tfstate"
+    key    = "terraform-aws-website.tfstate"
     region = "eu-west-1"
   }
 }
@@ -18,13 +18,13 @@ provider "aws" {}
 
 module "cloudfront" {
   dns_zone = "Z2TMUMYK9QCMW0"
-  owner = "${var.owner}"
+  owner    = "${var.owner}"
   source   = "./cloudfront"
 }
 
 module "ec2" {
   deploy_key = "${var.deploy_key}"
-  dns_zone = "Z2TMUMYK9QCMW0"
-  owner = "${var.owner}"
-  source = "./ec2"
+  dns_zone   = "Z2TMUMYK9QCMW0"
+  owner      = "${var.owner}"
+  source     = "./ec2"
 }
